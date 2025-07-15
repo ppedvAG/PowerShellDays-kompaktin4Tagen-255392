142b4c692d51afe9c5170acb6aa3d26a9eca39a4
Stefan Ober <StefanO@ppedv.de>
2025-07-15 16:08:19 +0200

    Skriptbeispiel

---
+++ Hilfreiches/Skriptbeispiel.ps1
@@ -0,0 +1,14 @@
+﻿param(
+[Parameter(Mandatory=$true)]
+[ValidateSet(4624,4625,4634)]
+[int]$EventId,
+
+[ValidateRange(5,10)]
+[int]$Newest = 3,
+
+[ValidateScript({Test-NetConnection -ComputerName $PSItem -CommonTcpPort WinRm -InformationLevel Quiet})]
+[string]$computername = "localhost"
+)
+
+Get-WinEvent -LogName Security -ComputerName $computername | Where-Object Id -eq $EventId | Select-Object -First $Newest
+
